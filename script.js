0284780d4017292bcf7b8cfe0d9a3e2ed090c0a3
Oleg Blonnyi <olegblonnyi11042005@gmail.com>
2024-11-11 12:51:58 +0200

    Testing Form

--- script.js
+++ script.js
@@ -1,33 +1,96 @@
-document.querySelector('form').addEventListener('submit', async (event) => {
-  event.preventDefault();
+// логіка форми
 
-  const formData = {
-      userName: document.getElementById('userName').value,
-      phoneNumber: document.getElementById('phoneNumber').value,
-      email: document.getElementById('email').value,
-      nickTG: document.getElementById('nickTG').value
-  };
+// service_uerq9aj // --- Серві ID
+// template_t99vnli // template id
+// eQS6CebShZKZGETRn // publick key
 
-  // Створюємо запит для GitHub API
-  try {
-      const response = await fetch('https://api.github.com/repos/OlegBlonnyi/Testing-DB/contents/data.json', {
-          method: 'PUT',
-          headers: {
-              'Authorization': `Bearer DB_FOR_USERS`, // заміни на свій токен
-              'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-              message: 'Додано новий запис даних',
-              content: btoa(JSON.stringify(formData, null, 2)) // кодуємо дані у base64
-          })
-      });
-      if (response.ok) {
-          alert("Дані успішно відправлені");
-      } else {
-          alert("Помилка під час відправки даних");
-      }
-  } catch (error) {
-      console.error("Error:", error);
-      alert("Помилка з'єднання");
+(function () {
+  emailjs.init({
+    // publicKey: "eQS6CebShZKZGETRn", // BODI
+    publicKey: "_eP-bKXVm1q1Qduzr",
+  });
+})();
+
+const phoneInput = document.getElementById("phoneNumber");
+phoneInput.addEventListener("focus", () => {
+  if (!phoneInput.value) {
+    phoneInput.value = "+380";
   }
 });
+phoneInput.addEventListener("blur", () => {
+  if (phoneInput.value === "+380") {
+    phoneInput.value = "";
+  }
+});
+
+const buyerForm = document.forms.buyerForm;
+
+const nameError = document.getElementById("nameError");
+const phoneError = document.getElementById("phoneError");
+const emailError = document.getElementById("emailError");
+
+buyerForm.addEventListener("submit", function (e) {
+  nameError.innerText = "";
+  phoneError.innerText = "";
+  emailError.innerText = "";
+  e.preventDefault();
+
+  let validateUserName = () => {
+    if (buyerForm.userName.value == "") {
+      nameError.innerText = "Це поле є обовязковим!";
+      return false;
+    }
+    if (buyerForm.userName.value.length > 35) {
+      nameError.innerText = "Максимальна кількість символів 35";
+      return false;
+    }
+    return true;
+  };
+
+  let validatePhoneNumber = () => {
+    if (buyerForm.phoneNumber.value == "") {
+      phoneError.innerText = "Це поле є обовязковим!";
+      return false;
+    }
+    if (buyerForm.phoneNumber.value.length != 13) {
+      phoneError.innerText = "Неправильний формат номеру";
+      return false;
+    }
+    return true;
+  };
+  let validateEmail = (e) => {
+    let pattern = /^[a-zA-Z][a-zA-z._0-9]+@[a-z0-9]{3,20}\.[a-z]{2,4}$/
+    if (!pattern.test(buyerForm.email.value)) {
+      if(buyerForm.email.value == "") {
+        emailError.innerText = "Ви не ввели email"
+        return false
+      }
+      emailError.innerText = "Неправильно введений email"
+      return false
+    } 
+    return true
+  }
+
+  if (!validateUserName() || !validatePhoneNumber() || !validateEmail()) return false;
+  
+  const formData = {
+    userName: document.getElementById("userName").value,
+    phoneNumber: document.getElementById("phoneNumber").value,
+    email: document.getElementById("email").value,
+    nickTG: document.getElementById("nickTG").value || "Не вказано",
+  };
+
+  // emailjs.send('service_uerq9aj', 'template_t99vnli', formData) // BODI
+  emailjs.send("service_jy4vky7", "template_xtf1sse", formData).then(
+    function (response) {
+      console.log("Успіх:", response);
+      alert("Дякуємо за реєстрацію! Лист відправлено.");
+    },
+    function (error) {
+      console.log("Помилка:", error);
+      alert("Сталася помилка при відправці листа. Спробуйте ще раз.");
+    }
+  );
+});
+
+// логіка форми
